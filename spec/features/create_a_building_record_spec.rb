f7dfcf3dfbbfb8235ad1ca70a194db330e0c5b1e
Richard Davis <rdavis.bacs@gmail.com>
2013-09-26 12:47:16 -0400

    wrote first tests

--- spec/features/create_a_building_record_spec.rb
+++ spec/features/create_a_building_record_spec.rb
@@ -24,9 +24,25 @@ feature 'create a building record', %q{
   # I can record another building.
   #
 
-  scenario 'user enters valid information'
+  scenario 'user enters valid information' do
+    previous_count = Building.count
+
+    visit new_building_path
+    fill_in 'Address', with: '13 Elm Street'
+    fill_in 'City', with: 'Canton'
+    fill_in 'State', with: 'MA'
+    fill_in 'Zip', with: '02021'
+    fill_in 'Description', with: 'Quite a frightful place.'
+    click_button 'Add Building'
+
+    expect(page).to have_content('New Building Added')
+    expect(Building.count).to eql(previous_count + 1)
+  end
 
 
-  scenario 'user enters invalid information'
+  scenario 'user enters invalid information' do
+    visit new_building_path
+    expect(page).to have_content("can't be blank")
+  end
 
 end
